af41a72e056d6016c1c50ab19946965429d90b61
Aleksandar Gakovic <algakovic@gmail.com>
2020-12-08 22:13:05 +0000

    fix typo in open function for admins from admins to adminlist. order shuffle function to be with other shuffle functions and inprove comments throughout the autosorter.py file

--- Autosorter.py
+++ Autosorter.py
@@ -4,9 +4,9 @@ from random import shuffle
 # Import admin and participant data from txt file:
 def among_us_lobby_sorter():
     '''
-    admins must be included in the participants list
-    the number of participants must be divisible by 10
-    Have an even amount of admins in the list. A pair for each lobby is good.
+    * Admins must be included in the participants list. Their names must match like for like.
+    * The number of participants must be divisible by 10
+    Having even number of admins is not necessary but a pair for each lobby at least is good.
 
     The auto-sorter will sort participants 
     into lobbies of 10 and standby.txt players will go 
@@ -15,22 +15,24 @@ def among_us_lobby_sorter():
     
     with open('uploads/participants.txt', 'r') as party, open('uploads/admins.txt', 'r') as adminlist,   open('uploads/standby.txt', 'r') as standby:
         participants = [s.strip() for s in party.readlines()]
-        admins = [s.strip() for s in party.readlines()]
+        admins = [s.strip() for s in adminlist.readlines()]
         standby = [s.strip() for s in standby.readlines()]
-        
-    # Make a copy of participant list for shuffling
+    
+    # Make a copy of admin and participants list and shuffle
     shuffled_participants = participants.copy()
+    shuffle(shuffled_participants)
     
     shuffle(admins)
     admins_copy = admins.copy()
     
+    # Create magic variable based on participant number:
     n = int(len(participants)/10)
     
     # No. of lobbies created depends on participants:
     index = list(range(n+1))    
     lobbies = [[] for i in index]
     
-    #add admins to lobbies:
+    # seed admins to lobbies:
     while len(admins_copy) > 0:
         for lobby in lobbies[:n]:
             lobby.append(admins_copy.pop())
@@ -38,20 +40,11 @@ def among_us_lobby_sorter():
                 continue
             else:
                 break
-   
-    admins = list(filter(None, admins))
-      
-    
-    
+  
     for admin in admins:  
         shuffled_participants.remove(admin)
 
-    
-    shuffle(shuffled_participants)
-    
-    
-    
-    #add remaining participants to lobbies:
+    # add remaining participants to lobbies:
     full = []
     while len(shuffled_participants) > 0:
         for lobby in lobbies[:n]:
@@ -67,7 +60,7 @@ def among_us_lobby_sorter():
             else:
                 break
             
-    # standby participants:
+    # add standby participants:
     while len (standby)>0:
         lobbies[n].append(standby.pop())
         if len(standby)>0:
@@ -75,5 +68,4 @@ def among_us_lobby_sorter():
         else:
             break
             
-    lobbies = list(filter(None, lobbies))
     return lobbies
